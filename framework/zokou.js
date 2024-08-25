53de08ec482988123e8a7941d50da3cecd8c9334
carl24tech <cwilly403@gmail.com>
2024-08-25 21:55:43 +0300

    Cyberion-V1☢️

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🚘";
+        infoComs.reaction = "☣️";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
