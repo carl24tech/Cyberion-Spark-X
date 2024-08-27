a82a11d5a771363f4c981e7957297b95282f6a82
carl24tech <cwilly403@gmail.com>
2024-08-27 19:34:59 +0300

    Cyberion-V1☢️

---
+++ commandes/scan.js
@@ -0,0 +1,86 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "scan", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+ *Tap on the link to get session*
+ 
+https://github.com/Carl165/SESSION_SITE 
+ 
+ *STEPS TO GET SESSION*
+ 
+ 1. Open link above
+ 2. Enter your whatsapp number with your country code eg : 254,255. And tap on submit
+ 3. Trex Md will send you a code. Copy that code. Then whatsapp will sent Notification
+ 4. Tap on that notification then enter in the code that Trex sent you.
+ 5. It will load for sometime then Trex will send A long session to your inbox on whatsapp at your own number
+ 6. Copy that long session and send it to your deployer or deploy it yourself.
+ 
+ 
+ `;
+    
+let menuMsg = `
+> Made by : 𝗖𝗔𝗥𝗟𝗧𝗘𝗖𝗛
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bugatti*, déveloper Marisel" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bugatti*, déveloper Marisel" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+}); 
+
+                    
