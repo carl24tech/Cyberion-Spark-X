886134a8da9a6263bb6cdbf6d81225f6fd8794bb
carl24tech <cwilly403@gmail.com>
2024-08-31 21:45:56 +0300

    Cyberion-V1☢️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/kingCarl.js
+++ commandes/kingCarl.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "william", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
