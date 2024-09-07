6e65ab66e2a218cd446361a08d0c00480ee86058
carl24tech <cwilly403@gmail.com>
2024-09-07 14:03:22 +0300

    Cyberion-V1☢️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/info.js
+++ commandes/info.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "gitdata", catégorie:"General", reaction: "📑", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/info/carl24tech/Cyberion-V1';
+  const githubRepo = 'https://api.github.com/repos/betingrich/Tesla';
   const img = 'https://telegra.ph/file/0e2976b99ebfc22a2753f.jpg';
 
   try {
@@ -22,13 +22,13 @@ zokou({ nomCom: "gitdata", catégorie:"General", reaction: "📑", nomFichier: _
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = `*𝐇𝐄𝐋𝐋𝐎 𝐀𝐌* *𝐂𝐘𝐁𝐄𝐑𝐈𝐎𝐍-𝐕1.*\n  
-🏎️ *𝗣𝗮𝗶𝗿 𝗰𝗼𝗱* https://github.com/carl24tech/SESSION-SITE
-🏎️ *𝗥𝗲𝗽𝗼:* ${data.html_url}
-🏎️ *𝗦𝘁𝗮𝗿𝘀:* ${repoInfo.stars}
-🏎️ *𝗙𝗼𝗿𝗸𝘀:* ${repoInfo.forks}
-🏎️ *𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:* ${releaseDate}
-🏎️ *𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗼𝗻:* ${repoInfo.lastUpdate}
-🏎️ *𝗢𝘄𝗻𝗲𝗿:* *CARLTECH*
+💬 *𝗣𝗮𝗶𝗿 𝗰𝗼𝗱* https://github.com/carl24tech/SESSION-SITE
+🗯 *𝗥𝗲𝗽𝗼:* ${data.html_url}
+💫 *𝗦𝘁𝗮𝗿𝘀:* ${repoInfo.stars}
+🍽 *𝗙𝗼𝗿𝗸𝘀:* ${repoInfo.forks}
+💥 *𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:* ${releaseDate}
+🗨 *𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗼𝗻:* ${repoInfo.lastUpdate}
+🕵 *𝗢𝘄𝗻𝗲𝗿:* *CARLTECH*
 __________________________________
             *𝐂𝐘𝐁𝐄𝐑𝐈𝐎𝐍*`;
 
