ceb12c85af36197f289b4dfefd7e56a73576ee28
carl24tech <cwilly403@gmail.com>
2024-09-07 14:09:08 +0300

    Cyberion-V1☢️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/info.js
+++ commandes/info.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "gitdata", catégorie:"General", reaction: "📑", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/betingrich/Tesla';
+  const githubRepo = 'https://api.github.com/repos/carl24tech/Cyberion-V1;
   const img = 'https://telegra.ph/file/0e2976b99ebfc22a2753f.jpg';
 
   try {
