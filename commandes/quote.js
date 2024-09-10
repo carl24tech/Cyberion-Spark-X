bbc67025156edd56747a4d3f7c5285e18734277c
carl24tech <cwilly403@gmail.com>
2024-09-10 11:12:21 +0300

    Cyberion-V1☢️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/quote.js
+++ commandes/quote.js
@@ -45,7 +45,7 @@
 
 
 
-const { zokou } = require('../framework/zokou');
+/*const { zokou } = require('../framework/zokou');
 
 zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, verifGroupe, arg } = commandeOptions;
