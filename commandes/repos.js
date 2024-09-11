7af31349da3ba63fa281c75f1239056c67a92bec
carl24tech <cwilly403@gmail.com>
2024-09-11 10:17:43 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/repos.js
+++ commandes/repos.js
@@ -18,7 +18,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "carltech", catégorie:"General", reaction: "🔎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repo", catégorie:"General", reaction: "🕳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/carl24tech/Cyberion-V1';
   const img = 'https://telegra.ph/file/0e2976b99ebfc22a2753f.jpg';
 
@@ -39,17 +39,25 @@ zokou({ nomCom: "carltech", catégorie:"General", reaction: "🔎", nomFichier: 
 
       const gitdata = `*𝐇𝐞𝐥𝐥𝐨 𝐦𝐲 𝐄𝐬𝐭𝐞𝐞𝐦𝐞𝐝 𝐮𝐬𝐞𝐫.𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐂𝐲𝐛𝐞𝐫𝐢𝐨𝐧 𝐕1 𝐢𝐧𝐟𝐨*\n  
       *𝐓𝐡𝐞 𝐚𝐛𝐨𝐯𝐞 𝐢𝐧𝐟𝐨 𝐢𝐬 𝐟𝐨𝐫 𝐲𝐨𝐮 𝐭𝐨 𝐤𝐧𝐨𝐰.*
-╭──▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱
-│⛲️│ *𝐒𝐞𝐬𝐬𝐢𝐨𝐧* https://github.com/carl24tech/SESSION-SITE
-│♨️│ *𝐑𝐞𝐩𝐨:* ${data.html_url}
-│🌟│ *𝐒𝐭𝐚𝐫𝐬:* ${repoInfo.stars}
-│🍽│ *𝐅𝐨𝐫𝐤𝐬:* ${repoInfo.forks}
-│⚯️│ *𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐃𝐚𝐭𝐞:* ${releaseDate}
-│🕳│ *𝐔𝐩𝐝𝐚𝐭𝐞𝐝: ${repoInfo.lastUpdate}
-│🕵️│ *𝐎𝐰𝐧𝐞𝐫:* 𝐂𝐀𝐑𝐋𝐓𝐄𝐂𝐇
-│🪀│ *𝐂𝐡𝐚𝐧𝐧𝐞𝐥:* https://whatsapp.com/channel/0029Vak0genJ93wQXq3q6X3h
-│📩│ *𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦:* https://t.me.carlltecch 
-╰──▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+╭──▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰
+╽ ⛲  *𝐒𝐞𝐬𝐬𝐢𝐨𝐧* https://github.com/carl24tech/SESSION-SITE
+┃ 
+┃ ♨️  *𝐑𝐞𝐩𝐨:* ${data.html_url}
+┃ 
+┃ 🌟  *𝐒𝐭𝐚𝐫𝐬:* ${repoInfo.stars}
+┃
+┃ 🍽  *𝐅𝐨𝐫𝐤𝐬:* ${repoInfo.forks}
+┃
+┃ ⚯️  *𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐃𝐚𝐭𝐞:* ${releaseDate}
+┃
+┃ 🕳  *𝐔𝐩𝐝𝐚𝐭𝐞𝐝: ${repoInfo.lastUpdate}
+┃
+┃ 🕵️  *𝐎𝐰𝐧𝐞𝐫:* 𝐂𝐀𝐑𝐋𝐓𝐄𝐂𝐇
+┃
+┃ 🪀  *𝐂𝐡𝐚𝐧𝐧𝐞𝐥:* https://whatsapp.com/channel/0029Vak0genJ93wQXq3q6X3h
+┃
+╿ 📩  *𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦:* https://t.me.carlltecch 
+╰──▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰
           *CYBERION-V1*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
