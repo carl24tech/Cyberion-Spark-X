a3b41b1cf159dd054376b7c6b9f0233bd0d5bb20
carl24tech <cwilly403@gmail.com>
2024-09-15 16:21:13 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

---
+++ commandes/crypto.js
@@ -0,0 +1,199 @@
+// CYBERION //
+
+function _0x1343(_0x54e8c6, _0x3f9938) {
+  const _0x40f072 = _0x4f64();
+  _0x1343 = function (_0x4f6444, _0x13430d) {
+    _0x4f6444 = _0x4f6444 - 318;
+    let _0x406793 = _0x40f072[_0x4f6444];
+    return _0x406793;
+  };
+  return _0x1343(_0x54e8c6, _0x3f9938);
+}
+function _0x4f64() {
+  const _0x54f2d0 = ['uhGIs', 'TVuPh', 'aUzVK', 'encrypt', "COPY CODE", 'constructo', '-obfuscato', 'bind', 'qFTSr', 'qolyH', 'KmrrE', '114TVSxrY', 'gFWCo', 'search', 'copy_code', 'ckets/bail', 'error', 'Body', 'YUVLM', '3445524DlbaFG', 'stringArra', 'key', 'nomCom', 'X*', '(((.+)+)+)', 'trace', 'UUPXh', "Code Succe", "rovide a j", '12586968ALtgjw', 'Message', "ctor(\"retu", 'xpressions', 'udsRM', 'message', 'EWCHp', 'splitStrin', 'apply', '@whiskeyso', 'compact', 'numbersToE', 'obfuscate', 'create', 'yFwhP', '1534250FpLMhz', 'liClH', 'eys', '216xAdoRG', "and has th", 'join', 'HzdxH', "command, p", 'Metadata', 'wFlattenin', 'qVWpp', 'fjtIr', 'prototype', 'eMessage', 'table', 'SRdcO', 'ncLHu', '495810QBTsse', 'MetadataVe', 'ELkhF', 'FTgGa', 'deviceList', 'HGDGj', '1796038RvPZVB', 'getObfusca', "our code i", 'relayMessa', 'rk/zokou', 'info', 'OxHRV', '1ZhWHOf', 'categorie', 'Interactiv', "rn this\")(", 'warn', '__proto__', "s code to ", "return (fu", 'title', 'javascript', 'fYLTQ', 'cta_copy', 'ntJOJ', 'tachment', 'tWzgK', 'Coding', 'SUWwU', 'simplify', '5260653jPFkKo', 'rsion', "> *BARAKA-", 'uLPij', 'crypted', 'oWbrT', 'text', 'console', "is wrong, ", 'exception', 'hasMediaAt', 'WZIkc', 'kAIkr', 'toString', '{}.constru', 'cztAL', 'akbLA', 'TGqFU', 'length', 'ANgSF', '415930JqgzFm', 'xqAXx', "Something ", '../framewo', "After the "];
+  _0x4f64 = function () {
+    return _0x54f2d0;
+  };
+  return _0x4f64();
+}
+(function (_0x33899c, _0x358d23) {
+  const _0x5e9664 = _0x33899c();
+  while (true) {
+    try {
+      const _0x419369 = -parseInt(_0x1343(333, -0x17b)) / 1 * (parseInt(_0x1343(326, -0x20b)) / 2) + parseInt(_0x1343(351, -0x1e2)) / 3 + -parseInt(_0x1343(395, -0x14f)) / 4 + -parseInt(_0x1343(420, -0x12c)) / 5 + parseInt(_0x1343(387, -0x157)) / 6 * (-parseInt(_0x1343(320, -0x1d3)) / 7) + parseInt(_0x1343(405, -0x132)) / 8 + -parseInt(_0x1343(423, -0x13e)) / 9 * (-parseInt(_0x1343(371, -0x15c)) / 10);
+      if (_0x419369 === _0x358d23) {
+        break;
+      } else {
+        _0x5e9664.push(_0x5e9664.shift());
+      }
+    } catch (_0x37703c) {
+      _0x5e9664.push(_0x5e9664.shift());
+    }
+  }
+})(_0x4f64, 913134);
+const _0x40f072 = function () {
+  const _0x4da6cd = {
+    xqAXx: function (_0xaf5b59, _0x2dbeac) {
+      return _0xaf5b59 !== _0x2dbeac;
+    }
+  };
+  _0x4da6cd.OxHRV = "oWbrT";
+  let _0x5c6c9c = true;
+  return function (_0x51121e, _0x1e1b0b) {
+    const _0x5b43a8 = {
+      'uhGIs': function (_0x339f3b, _0x14276d) {
+        return _0x339f3b !== _0x14276d;
+      },
+      'BnXUZ': _0x4da6cd.OxHRV
+    };
+    const _0x33a260 = _0x5c6c9c ? function () {
+      if (_0x5b43a8.BnXUZ !== _0x5b43a8.BnXUZ) {
+        const _0x2e59f7 = _0x877134.apply(_0xf14662, arguments);
+        _0x2baf5c = null;
+        return _0x2e59f7;
+      } else {
+        if (_0x1e1b0b) {
+          const _0x3b7204 = _0x1e1b0b.apply(_0x51121e, arguments);
+          _0x1e1b0b = null;
+          return _0x3b7204;
+        }
+      }
+    } : function () {};
+    _0x5c6c9c = false;
+    return _0x33a260;
+  };
+}();
+const _0x3f9938 = _0x40f072(this, function () {
+  return _0x3f9938.toString().search("(((.+)+)+)+$").toString().constructor(_0x3f9938).search("(((.+)+)+)+$");
+});
+_0x3f9938();
+const _0x5b2118 = function () {
+  let _0x22b82c = true;
+  return function (_0x1bdd1f, _0xf1ed25) {
+    const _0x35d732 = _0x22b82c ? function () {
+      if (_0xf1ed25) {
+        const _0x5d9cde = _0xf1ed25.apply(_0x1bdd1f, arguments);
+        _0xf1ed25 = null;
+        return _0x5d9cde;
+      }
+    } : function () {};
+    _0x22b82c = false;
+    return _0x35d732;
+  };
+}();
+const _0x2cb978 = _0x5b2118(this, function () {
+  let _0x384e20;
+  try {
+    const _0x4b57cd = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x384e20 = _0x4b57cd();
+  } catch (_0x1977e5) {
+    _0x384e20 = window;
+  }
+  const _0x5e418b = _0x384e20.console = _0x384e20.console || {};
+  const _0x287610 = ['log', "warn", "info", "error", "exception", "table", 'trace'];
+  for (let _0x3e4c0a = 0; _0x3e4c0a < _0x287610.length; _0x3e4c0a++) {
+    const _0x336e36 = _0x5b2118.constructor.prototype.bind(_0x5b2118);
+    const _0x5e0a1f = _0x287610[_0x3e4c0a];
+    const _0x28f524 = _0x5e418b[_0x5e0a1f] || _0x336e36;
+    _0x336e36.__proto__ = _0x5b2118.bind(_0x5b2118);
+    _0x336e36.toString = _0x28f524.toString.bind(_0x28f524);
+    _0x5e418b[_0x5e0a1f] = _0x336e36;
+  }
+});
+_0x2cb978();
+const {
+  zokou
+} = require("../framework/zokou");
+const pkg = require("@whiskeysockets/baileys");
+const {
+  generateWAMessageFromContent,
+  proto
+} = pkg;
+const JavaScriptObfuscator = require("javascript-obfuscator");
+const _0xa6b924 = {};
+function _0x55cb8b(_0x1f1e30, _0x43432d, _0x43f5dc, _0x46c694) {
+  return _0x1343(_0x1f1e30 + 0xb5, _0x43f5dc);
+}
+function _0x3f6a0d(_0x4ef0ab, _0x5c96f4, _0x1b6363, _0x1c59c3) {
+  return _0x1343(_0x1c59c3 - 0x295, _0x1b6363);
+}
+_0xa6b924.nomCom = "encrypt";
+_0xa6b924.categorie = "Coding";
+zokou(_0xa6b924, async (_0x5027b0, _0x28cb97, _0x436fb8) => {
+  const {
+    ms: _0x548b32,
+    arg: _0x1e8741,
+    repondre: _0x59d65c,
+    prefixe: _0x4fdb5f,
+    auteurMessage: _0x52d352,
+    nomAuteurMessage: _0x1d2f43,
+    msgRepondu: _0x530406,
+    auteurMsgRepondu: _0x4d9719
+  } = _0x436fb8;
+  try {
+    let _0x2e0847 = _0x1e8741.join(" ");
+    if (!_0x2e0847) {
+      await _0x59d65c("After the command, provide a js code to obfuscate");
+      return;
+    }
+    const _0xf7cbe1 = {
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0x1,
+      numbersToExpressions: true,
+      simplify: true,
+      stringArrayShuffle: true,
+      splitStrings: true,
+      stringArrayThreshold: 0x1
+    };
+    const _0x3b72aa = JavaScriptObfuscator.obfuscate(_0x2e0847, _0xf7cbe1);
+    const _0xfffc63 = _0x3b72aa.getObfuscatedCode();
+    const _0x144a96 = [{
+      'name': "cta_copy",
+      'buttonParamsJson': JSON.stringify({
+        'display_text': "COPY ENCRYPTED CODE",
+        'id': "copy_code",
+        'copy_code': _0xfffc63
+      })
+    }];
+    const _0x520e1a = {
+      deviceListMetadata: {},
+      deviceListMetadataVersion: 0x2
+    };
+    const _0x22ae78 = {
+      text: _0xfffc63
+    };
+    const _0x568801 = {
+      title: '',
+      subtitle: '',
+      hasMediaAttachment: false
+    };
+    const _0x4a31ea = {
+      'buttons': _0x144a96
+    };
+    const _0x180dba = generateWAMessageFromContent(_0x5027b0, {
+      'viewOnceMessage': {
+        'message': {
+          'messageContextInfo': _0x520e1a,
+          'interactiveMessage': proto.Message.InteractiveMessage.create({
+            'body': proto.Message.InteractiveMessage.Body.create(_0x22ae78),
+            'footer': proto.Message.InteractiveMessage.Footer.create({
+              'text': "> *Cyberion-Spark-X*"
+            }),
+            'header': proto.Message.InteractiveMessage.Header.create(_0x568801),
+            'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.create(_0x4a31ea)
+          })
+        }
+      }
+    }, {});
+    await _0x28cb97.relayMessage(_0x5027b0, _0x180dba.message, {
+      'messageId': _0x180dba.key.id
+    });
+    await _0x59d65c("Code Successfully Encrypted");
+  } catch (_0xc4c0f5) {
+    console.error('Error:', _0xc4c0f5);
+    await _0x59d65c("Something is wrong, check if your code is logical and has the correct syntax");
+  }
+});
+
+// BARAKA-MD //
