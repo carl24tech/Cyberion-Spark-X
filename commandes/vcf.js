9ae0d50f233d51bae41417ac74abb11a1c3ada02
carl24tech <cwilly403@gmail.com>
2024-09-15 17:47:55 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

---
+++ commandes/vcf.js
@@ -0,0 +1,144 @@
+(function (_0x4d6a8c, _0x240b1e) {
+  const _0x4a3458 = _0x4d6a8c();
+  while (true) {
+    try {
+      const _0x1534f1 = -parseInt(_0x5c95(401, 0x250)) / 1 * (-parseInt(_0x5c95(463, 0x21d)) / 2) + -parseInt(_0x5c95(410, 0x488)) / 3 * (-parseInt(_0x5c95(413, 0x4f4)) / 4) + -parseInt(_0x5c95(434, 0x272)) / 5 + parseInt(_0x5c95(382, 0x206)) / 6 * (-parseInt(_0x5c95(449, 0x253)) / 7) + parseInt(_0x5c95(420, 0x23b)) / 8 + parseInt(_0x5c95(409, 0x249)) / 9 * (parseInt(_0x5c95(476, 0x264)) / 10) + -parseInt(_0x5c95(443, 0x238)) / 11 * (parseInt(_0x5c95(391, 0x472)) / 12);
+      if (_0x1534f1 === _0x240b1e) {
+        break;
+      } else {
+        _0x4a3458.push(_0x4a3458.shift());
+      }
+    } catch (_0x4274b8) {
+      _0x4a3458.push(_0x4a3458.shift());
+    }
+  }
+})(_0x31a7, 454861);
+function _0x5c95(_0x4b9d08, _0x1e593f) {
+  const _0x3b0cc8 = _0x31a7();
+  _0x5c95 = function (_0x31a78c, _0x5c95ba) {
+    _0x31a78c = _0x31a78c - 355;
+    let _0x2e5a49 = _0x3b0cc8[_0x31a78c];
+    return _0x2e5a49;
+  };
+  return _0x5c95(_0x4b9d08, _0x1e593f);
+}
+function _0x28667a(_0x557e5e, _0x117a74, _0x3e7a9b, _0x410732) {
+  return _0x5c95(_0x3e7a9b + 0x274, _0x117a74);
+}
+const _0x3b0cc8 = function () {
+  let _0x3231ed = true;
+  return function (_0x2fed24, _0x199761) {
+    const _0x3429e8 = _0x3231ed ? function () {
+      if (_0x199761) {
+        const _0xc933de = _0x199761.apply(_0x2fed24, arguments);
+        _0x199761 = null;
+        return _0xc933de;
+      }
+    } : function () {};
+    _0x3231ed = false;
+    return _0x3429e8;
+  };
+}();
+const _0x1e593f = _0x3b0cc8(this, function () {
+  return _0x1e593f.toString().search("(((.+)+)+)+$").toString().constructor(_0x1e593f).search("(((.+)+)+)+$");
+});
+function _0x31a7() {
+  const _0x516404 = ['bind', 'eys', 'console', 'RFyfE', "e creating", 'loMmr', 'HDIKY', 'MEEzj', '3377532cEZkAK', 'trim', 'QAJOv', 'LeJcw', 'cSrlI', 'constructo', 'MqqDs', 'oMTPM', '...', "\nTotal Con", '2VxxMLf', 'RXKhU', "\nTEL;type=", 'xPVWf', 'writeFileS', 'GptjI', "nction() ", 'iMtjG', '2133ZRebaP', '51rySsPw', "tacts: ", 'warn', '160708yvDLZl', 'YSkga', 'KebhQ', 'jyTDI', 'info', 'message', 'categorie', '2908600gCGdgG', 'MKwzr', 'YcJXX', "return (fu", 'XgnZF', 'MiUth', " here!", 'VOICE;waid', 'name', "n groups o", 'log', "t an admin", "ng or send", 'zSzAi', '2285765EeDrOK', '../framewo', 'default', 'rk/zokou', 'toString', 'subject', 'split', 'DVNfF', 'exception', '22ZdPNeo', 'notify', 'table', 'XsTsK', 'CELL;type=', './contacts', '221277udsqOA', 'ckets/bail', 'VIQWV', '(((.+)+)+)', 'xYkqZ', 'juvFp', 'CfTbg', '.Vcf', 'text/vcard', 'qkJxw', "You are no", "D\nVERSION:", 'nomCom', "VCF for ", '128418pvuneL', 'rCekF', "Bot is Com", 'gZqZQ', 'vcf', "Error whil", "F. Please ", 'zkOGz', 'trace', 'PtZsM', "into a vcf", 'Ldvfi', 'glqIG', '35330SWzlUu', 'nly', "try again.", " contacts ", "ctor(\"retu", 'apply', 'yKzWc', 'VxIQm', "3.0\nFN:", 'quoted', "ing the VC", "ile creati", 'Group', 'kBjTH', 'PFvfb', "An error o", 'Mropu', "g VCF:", 'xpiration', "[BARAKA] +", "ccurred wh", 'prototype', 'BEGIN:VCAR', 'length', 'search', 'error', " or sendin", 'bBMVi', 'tjQrT', '{}.constru', "A moment, ", '@whiskeyso', '102nwbdVv'];
+  _0x31a7 = function () {
+    return _0x516404;
+  };
+  return _0x31a7();
+}
+_0x1e593f();
+const _0x14565e = function () {
+  let _0x3fbbf1 = true;
+  return function (_0xb1d37d, _0x2b822c) {
+    const _0x4221ad = _0x3fbbf1 ? function () {
+      if (_0x2b822c) {
+        const _0x37785d = _0x2b822c.apply(_0xb1d37d, arguments);
+        _0x2b822c = null;
+        return _0x37785d;
+      }
+    } : function () {};
+    _0x3fbbf1 = false;
+    return _0x4221ad;
+  };
+}();
+const _0xc3d488 = _0x14565e(this, function () {
+  let _0x3018b6;
+  try {
+    const _0x3d84f3 = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x3018b6 = _0x3d84f3();
+  } catch (_0x49d276) {
+    _0x3018b6 = window;
+  }
+  const _0x2c3f04 = _0x3018b6.console = _0x3018b6.console || {};
+  const _0x1fa591 = ["log", "warn", "info", "error", "exception", "table", "trace"];
+  for (let _0x268e66 = 0; _0x268e66 < _0x1fa591.length; _0x268e66++) {
+    const _0x21aea1 = _0x14565e.constructor.prototype.bind(_0x14565e);
+    const _0x5dd7cb = _0x1fa591[_0x268e66];
+    const _0x1f85b4 = _0x2c3f04[_0x5dd7cb] || _0x21aea1;
+    _0x21aea1.__proto__ = _0x14565e.bind(_0x14565e);
+    _0x21aea1.toString = _0x1f85b4.toString.bind(_0x1f85b4);
+    _0x2c3f04[_0x5dd7cb] = _0x21aea1;
+  }
+});
+function _0x5e826a(_0x7e8476, _0x35150c, _0x302386, _0x5a7164) {
+  return _0x5c95(_0x35150c + 0x37b, _0x5a7164);
+}
+_0xc3d488();
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  getBinaryNodeChild,
+  getBinaryNodeChildren
+} = require("@whiskeysockets/baileys")["default"];
+const _0x4ca7ae = {
+  nomCom: "vcf",
+  categorie: "Group",
+  reaction:'📄'
+};
+zokou(_0x4ca7ae, async (_0x4f12b5, _0x50892c, _0x3b76b3) => {
+  const {
+    repondre: _0x30130d,
+    verifGroupe: _0x363f4e,
+    verifAdmin: _0x1fa15a,
+    ms: _0x1901f0
+  } = _0x3b76b3;
+  const _0x74e876 = require('fs');
+  if (!_0x1fa15a) {
+    _0x30130d("Lol..I need an admin position to do this!");
+    return;
+  }
+  if (!_0x363f4e) {
+    _0x30130d("Huhh dear this command is only for groups");
+    return;
+  }
+  try {
+    let _0x4f970c = await _0x50892c.groupMetadata(_0x4f12b5);
+    const _0x44540a = await _0x4f970c.participants;
+    let _0x1fa993 = '';
+    for (let _0x13c370 of _0x44540a) {
+      let _0xe5877 = _0x13c370.id.split('@')[0];
+      let _0x2e3c5e = _0x13c370.name || _0x13c370.notify || "[BARAKA] +" + _0xe5877;
+      _0x1fa993 += "BEGIN:VCARD\nVERSION:3.0\nFN:" + _0x2e3c5e + "\nTEL;type=CELL;type=VOICE;waid=" + _0xe5877 + ':+' + _0xe5877 + "\nEND:VCARD\n";
+    }
+    await _0x30130d("Hold, Compiling in progress " + _0x44540a.length + " contacts into a vcf...");
+    await _0x74e876.writeFileSync("./contacts.vcf", _0x1fa993.trim());
+    const _0x44678f = {
+      ephemeralExpiration: 0x15180,
+      quoted: _0x1901f0
+    };
+    await _0x50892c.sendMessage(_0x4f12b5, {
+      'document': _0x74e876.readFileSync("./contacts.vcf"),
+      'mimetype': "text/vcard",
+      'fileName': _0x4f970c.subject + ".Vcf",
+      'caption': "VCF for " + _0x4f970c.subject + "\nTotal Contacts: " + _0x44540a.length
+    }, _0x44678f);
+    _0x74e876.unlinkSync("./contacts.vcf");
+  } catch (_0x2608c1) {
+    console.error("Error while creating or sending VCF:", _0x2608c1.message || _0x2608c1);
+    _0x30130d("An error occurred while creating or sending the VCF. Please try again.");
+  }
+});
