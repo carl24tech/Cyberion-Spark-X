e8c198339a951445fb76f18c4e31320c7451aaf0
carl24tech <cwilly403@gmail.com>
2024-09-25 15:27:46 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- commandes/cont.js
+++ commandes/cont.js
@@ -6,26 +6,72 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const _0x5c956b=_0x5942;(function(_0x58ecd4,_0x515d25){const _0x5d9657=_0x5942,_0x3dc865=_0x58ecd4();while(!![]){try{const _0x38fdb6=parseInt(_0x5d9657(0xed))/(-0x17cd+-0x26b*-0x7+0x3*0x24b)*(-parseInt(_0x5d9657(0xf1))/(0xb*0x312+0xe9*-0x1+-0xd*0x287))+parseInt(_0x5d9657(0xc5))/(-0xb5b+-0x1a09+-0x2567*-0x1)+-parseInt(_0x5d9657(0xd0))/(-0x1b17+0xe06*-0x2+-0x3727*-0x1)*(parseInt(_0x5d9657(0xf0))/(-0x1*0x1eb+0x8f1+-0x701))+parseInt(_0x5d9657(0x9d))/(-0x1e08+-0xb*-0x2dd+-0x1*0x171)*(parseInt(_0x5d9657(0xb1))/(-0x1c*0x1+-0x1201+0x1224))+parseInt(_0x5d9657(0xcb))/(-0xe35*-0x1+-0x1ce3+0xeb6)*(parseInt(_0x5d9657(0xa4))/(-0x2e*0xb1+-0x1d70+0x3d47))+parseInt(_0x5d9657(0xa1))/(0x164e+-0x1c2b+0x1*0x5e7)+-parseInt(_0x5d9657(0xe1))/(-0xe3*-0x1f+-0xeee+-0x12*0xb2)*(-parseInt(_0x5d9657(0x86))/(0x24f2+-0x5a*-0x1a+0xa6*-0x47));if(_0x38fdb6===_0x515d25)break;else _0x3dc865['push'](_0x3dc865['shift']());}catch(_0x302048){_0x3dc865['push'](_0x3dc865['shift']());}}}(_0x9b9d,0x269*0xa3c+-0x125c50+0x5*0x18cb9));function _0x5942(_0x3375c0,_0x42c679){const _0x55b82a=_0x9b9d();return _0x5942=function(_0x7a2363,_0x1b4ec3){_0x7a2363=_0x7a2363-(0xf0d+-0xa3d*0x2+0x5f0);let _0x3ab0dd=_0x55b82a[_0x7a2363];return _0x3ab0dd;},_0x5942(_0x3375c0,_0x42c679);}const util=require(_0x5c956b(0xba)),fs=require(_0x5c956b(0xe9)),{zokou}=require(__dirname+(_0x5c956b(0x9a)+_0x5c956b(0xdb))),{format}=require(__dirname+(_0x5c956b(0x9a)+_0x5c956b(0x94)+_0x5c956b(0xc0))),os=require('os'),moment=require(_0x5c956b(0xcf)+_0x5c956b(0xb4)),s=require(__dirname+_0x5c956b(0xd3)),more=String[_0x5c956b(0xb0)+'de'](0x2836+0x1*0x4f5+-0x45f*0x3),readmore=more[_0x5c956b(0x9f)](0x2*-0x10fc+0x8b*0x36+0x1447);zokou({'nomCom':_0x5c956b(0x84),'categorie':_0x5c956b(0xbc)},async(_0x4650a2,_0x56f43d,_0x5d82c9)=>{const _0x314594=_0x5c956b,_0xa3c9e4={'MZXQi':function(_0x5adfd5,_0x18d12f){return _0x5adfd5(_0x18d12f);},'wIvvn':function(_0xd6e1fb,_0x53a730){return _0xd6e1fb+_0x53a730;},'hbYdC':_0x314594(0x9a)+_0x314594(0xc3),'riSrH':_0x314594(0xa8),'yhdCA':function(_0x234101,_0x51b664){return _0x234101!=_0x51b664;},'fdSBa':_0x314594(0xe7),'evtAm':_0x314594(0xec),'kQCrA':_0x314594(0xa7)+_0x314594(0xa6),'KMasi':function(_0x52f322){return _0x52f322();},'peFDd':_0x314594(0xaa),'FjbVH':_0x314594(0xf7),'bYzWS':_0x314594(0xa2)+_0x314594(0xd5)+_0x314594(0xf3)+_0x314594(0xa0),'LFzmZ':_0x314594(0xd7)+_0x314594(0x9c),'gDJCU':function(_0x5c127f,_0x25174b){return _0x5c127f+_0x25174b;},'CumhB':function(_0x2361f1,_0x23d6b8){return _0x2361f1+_0x23d6b8;},'BaOzv':function(_0xb4a61c,_0xed2d47){return _0xb4a61c+_0xed2d47;},'xaHgm':function(_0x463ef5,_0x40c575){return _0x463ef5(_0x40c575);}};let {ms:_0x232eff,repondre:_0x4dc249,prefixe:_0xfe638c,nomAuteurMessage:_0x2058eb,mybotpic:_0x5ba3e1}=_0x5d82c9,{cm:_0x3883b9}=_0xa3c9e4[_0x314594(0xe0)](require,_0xa3c9e4[_0x314594(0x8d)](__dirname,_0xa3c9e4[_0x314594(0xae)]));var _0x279eb8={},_0x77d414=_0xa3c9e4[_0x314594(0x8e)];_0xa3c9e4[_0x314594(0xf2)](s[_0x314594(0x93)][_0x314594(0xf9)+_0x314594(0xe4)](),_0xa3c9e4[_0x314594(0x8c)])&&(_0x77d414=_0xa3c9e4[_0x314594(0xca)]);_0x3883b9[_0x314594(0x90)](async(_0x20bffc,_0x32df45)=>{const _0x6d96de=_0x314594;if(!_0x279eb8[_0x20bffc[_0x6d96de(0xce)]])_0x279eb8[_0x20bffc[_0x6d96de(0xce)]]=[];_0x279eb8[_0x20bffc[_0x6d96de(0xce)]][_0x6d96de(0xbf)](_0x20bffc[_0x6d96de(0xc8)]);}),moment['tz'][_0x314594(0xd1)](_0xa3c9e4[_0x314594(0xe8)]);const _0x195615=_0xa3c9e4[_0x314594(0x8a)](moment)[_0x314594(0xd2)](_0xa3c9e4[_0x314594(0xb7)]),_0x1201f1=_0xa3c9e4[_0x314594(0x8a)](moment)[_0x314594(0xd2)](_0xa3c9e4[_0x314594(0xde)]);let _0x5d8fe5=_0x314594(0x95)+_0x314594(0xf8)+_0x314594(0x88)+_0x314594(0x9b)+_0x314594(0xac)+_0x314594(0xb8)+_0x314594(0x87)+_0x314594(0x99)+_0x314594(0xc2)+_0x314594(0xdf)+_0x314594(0xc4)+_0x314594(0xbd)+_0x314594(0xc1)+_0x314594(0xb9)+_0x314594(0xb3)+_0x314594(0x96)+_0x314594(0xbb)+_0x314594(0xb6)+_0x314594(0x85)+_0x314594(0xcc)+_0x314594(0x8f)+_0x314594(0xd4)+_0x314594(0x92)+_0x314594(0xe5)+_0x314594(0x9e)+_0x314594(0x97)+_0x314594(0xeb)+_0x314594(0xa9)+_0x314594(0xc7)+_0x314594(0xcd)+_0x314594(0xd9)+_0x314594(0xda)+_0x314594(0xef)+_0x314594(0xf6)+_0x314594(0x89)+_0x314594(0xe3)+_0x314594(0xe2)+_0x314594(0xad)+_0x314594(0xd6)+_0x314594(0xe6)+_0x314594(0xdc)+_0x314594(0xb5)+_0x314594(0xf5)+_0x314594(0x8b)+_0x314594(0xc9)+_0x314594(0xb2)+_0x314594(0xc6)+_0x314594(0xb5)+_0x314594(0x83)+'\x0a\x20',_0x4bab96=_0x314594(0xa3)+_0x314594(0xd8)+_0x314594(0xab);var _0xd3033c=_0xa3c9e4[_0x314594(0x8a)](_0x5ba3e1);if(_0xd3033c[_0x314594(0x91)](/\.(mp4|gif)$/i))try{_0x56f43d[_0x314594(0x98)+'e'](_0x4650a2,{'video':{'url':_0xd3033c},'caption':_0xa3c9e4[_0x314594(0x8d)](_0x5d8fe5,_0x4bab96),'footer':_0xa3c9e4[_0x314594(0xee)],'gifPlayback':!![]},{'quoted':_0x232eff});}catch(_0x34ab92){console[_0x314594(0xa5)](_0xa3c9e4[_0x314594(0x8d)](_0xa3c9e4[_0x314594(0xea)],_0x34ab92)),_0xa3c9e4[_0x314594(0xe0)](_0x4dc249,_0xa3c9e4[_0x314594(0x8d)](_0xa3c9e4[_0x314594(0xea)],_0x34ab92));}else{if(_0xd3033c[_0x314594(0x91)](/\.(jpeg|png|jpg)$/i))try{_0x56f43d[_0x314594(0x98)+'e'](_0x4650a2,{'image':{'url':_0xd3033c},'caption':_0xa3c9e4[_0x314594(0xbe)](_0x5d8fe5,_0x4bab96),'footer':_0xa3c9e4[_0x314594(0xee)]},{'quoted':_0x232eff});}catch(_0x2cc7a1){console[_0x314594(0xa5)](_0xa3c9e4[_0x314594(0xdd)](_0xa3c9e4[_0x314594(0xea)],_0x2cc7a1)),_0xa3c9e4[_0x314594(0xe0)](_0x4dc249,_0xa3c9e4[_0x314594(0xaf)](_0xa3c9e4[_0x314594(0xea)],_0x2cc7a1));}else _0xa3c9e4[_0x314594(0xf4)](_0x4dc249,_0xa3c9e4[_0x314594(0xaf)](_0x5d8fe5,_0x4bab96));}});function _0x9b9d(){const _0x5427e0=['ork/mesfon','\x0a\x20*HELLO\x20D','the\x20develo','individual','sendMessag','𝐢𝐧𝐝𝐥𝐲\x20𝐤𝐧𝐨𝐰','/../framew','E\x20CONTRIBU','reur\x20','12xeXPEx','He\x20is\x20the\x20','repeat','arisel','13551800PBwlMC','Je\x20suis\x20*B','\x0a>\x20Made\x20by','11511RpLfoY','log','robi','Africa/Nai','public','up\x20with\x20a\x20','HH:mm:ss','𝗛\x0a','TORS\x20ON\x20TH','the\x20base\x20w','hbYdC','BaOzv','fromCharCo','4414235xGvwCz','RLTECH❒⁠⁠\x0a','k\x20part\x20in\x20','ezone','──────────','yberion\x20V1','peFDd','IS\x20PROJECT','n\x20that\x20too','util','pment\x20of\x20C','General','\x20●\x20The\x20Rig','gDJCU','push','ctions','ht\x20hand\x20ma','\x20𝐭𝐡𝐢𝐬*\x0a\x20\x0a\x20','ork//zokou','\x20Adams:\x0a\x20\x0a','111240IGuEjL','\x20⁠⁠⁠⁠╰────','great\x20idea','nomCom','⁠⁠⁠⁠\x0a\x20│\x20CA','evtAm','1448ppprpR','the\x20every\x20','\x20of\x20genera','categorie','moment-tim','220GbQKPT','setDefault','format','/../set','istance.\x0a\x20','ugatti*,\x20d','here\x20Cyber','🥵🥵\x20Menu\x20er','\x20:\x20𝗖𝗔𝗥𝗟𝗧𝗘𝗖','ting\x20Cyber','ion\x20V1.\x0a\x20\x0a','ork/zokou','s.\x0a\x20\x0a\x20╭───','CumhB','FjbVH','☆.\x20Ibrahim','MZXQi','22VbkXyn','ovider\x20of\x20','fficial\x20pr','werCase','ECH:\x0a\x20\x0a\x20●\x20','ion\x20V1\x20run','yes','kQCrA','fs-extra','LFzmZ','\x20who\x20came\x20','private','46327uXDMnh','bYzWS','\x20☆\x20Devibra','80405oYLaCe','70AEkWkS','yhdCA','éveloper\x20M','xaHgm','──────❒\x0a\x20│','ah:\x0a\x20\x0a\x20●\x20H','DD/MM/YYYY','EAR\x20THE\x20AB','toLocaleLo','─────❒\x0a\x20\x0a\x20','conts','\x20and\x20gave\x20','3251976WJnWSj','*\x0a\x20\x0a\x20\x0a\x20\x0a*𝐊','OVE\x20ARE\x20TH','e\x20is\x20the\x20o','KMasi','\x20CYBERION❒','fdSBa','wIvvn','riSrH','needed\x20ass','map','match','\x0a\x20★.\x20CARLT','MODE'];_0x9b9d=function(){return _0x5427e0;};return _0x9b9d();}
+const {
+  zokou
+} = require(__dirname + "/../framework/zokou");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+zokou({
+  'nomCom': "conts",
+  'categorie': "General"
+}, async (_0x4650a2, _0x56f43d, _0x5d82c9) => {
+  let {
+    ms: _0x232eff,
+    repondre: _0x4dc249,
+    prefixe: _0xfe638c,
+    nomAuteurMessage: _0x2058eb,
+    mybotpic: _0x5ba3e1
+  } = _0x5d82c9;
+  let {
+    cm: _0x3883b9
+  } = require(__dirname + "/../framework//zokou");
+  var _0x279eb8 = {};
+  var _0x77d414 = "public";
+  if (s.MODE.toLocaleLowerCase() != "yes") {
+    _0x77d414 = "private";
+  }
+  _0x3883b9.map(async (_0x20bffc, _0x32df45) => {
+    if (!_0x279eb8[_0x20bffc.categorie]) {
+      _0x279eb8[_0x20bffc.categorie] = [];
+    }
+    _0x279eb8[_0x20bffc.categorie].push(_0x20bffc.nomCom);
+  });
+  moment.tz.setDefault("Africa/Nairobi");
+  var _0xd3033c = _0x5ba3e1();
+  if (_0xd3033c.match(/\.(mp4|gif)$/i)) {
+    try {
+      _0x56f43d.sendMessage(_0x4650a2, {
+        'video': {
+          'url': _0xd3033c
+        },
+        'caption': "\n *HELLO DEAR THE ABOVE ARE THE CONTRIBUTORS ON THIS PROJECT*\n \n \n \n*𝐊𝐢𝐧𝐝𝐥𝐲 𝐤𝐧𝐨𝐰 𝐭𝐡𝐢𝐬*\n \n ☆. Ibraah:\n \n ● The Right hand man that took part in the development of Cyberion  and gave the every needed assistance.\n \n ★. CARLTECH:\n \n ● He is the individual who came up with a great idea of generating Cyberion.\n \n ☆ Devibraah:\n \n ● He is the official provider of the base where Cyberion runs.\n \n ╭───────────────────❒\n │ CYBERION❒⁠⁠⁠⁠\n │ CARLTECH❒⁠⁠\n ⁠⁠⁠⁠╰───────────────────❒\n \n \n \n> Made by : 𝗖𝗔𝗥𝗟𝗧𝗘𝗖𝗛\n",
+        'footer': "Je suis *Cyberion*, Carltech",
+        'gifPlayback': true
+      }, {
+        'quoted': _0x232eff
+      });
+    } catch (_0x34ab92) {
+      console.log("🥵🥵 Menu erreur " + _0x34ab92);
+      _0x4dc249("🥵🥵 Menu erreur " + _0x34ab92);
+    }
+  } else {
+    if (_0xd3033c.match(/\.(jpeg|png|jpg)$/i)) {
+      try {
+        _0x56f43d.sendMessage(_0x4650a2, {
+          'image': {
+            'url': _0xd3033c
+          },
+          'caption': "\n *HELLO DEAR THE ABOVE ARE THE CONTRIBUTORS ON THIS PROJECT*\n \n \n \n*𝐊𝐢𝐧𝐝𝐥𝐲 𝐤𝐧𝐨𝐰 𝐭𝐡𝐢𝐬*\n \n ☆. Ibraah:\n \n ● The Right hand man that took part in the development of Cyberion  and gave the every needed assistance.\n \n ★. CARLTECH:\n \n ● He is the individual who came up with a great idea of generating Cyberion.\n \n ☆ Devibraah:\n \n ● He is the official provider of the base where Cyberion  runs.\n \n ╭───────────────────❒\n │ CYBERION❒⁠⁠⁠⁠\n │ CARLTECH❒⁠⁠\n ⁠⁠⁠⁠╰───────────────────❒\n \n \n \n> Made by : 𝗖𝗔𝗥𝗟𝗧𝗘𝗖𝗛\n",
+          'footer': "Je suis *Bugatti*, déveloper Marisel"
+        }, {
+          'quoted': _0x232eff
+        });
+      } catch (_0x2cc7a1) {
+        console.log("🥵🥵 Menu erreur " + _0x2cc7a1);
+        _0x4dc249("🥵🥵 Menu erreur " + _0x2cc7a1);
+      }
+    } else {
+      _0x4dc249("\n *HELLO DEAR THE ABOVE ARE THE CONTRIBUTORS ON THIS PROJECT*\n \n \n \n*𝐊𝐢𝐧𝐝𝐥𝐲 𝐤𝐧𝐨𝐰 𝐭𝐡𝐢𝐬*\n \n ☆. Ibraah:\n \n ● The Right hand man that took part in the development of Cyberion  and gave the every needed assistance.\n \n ★. CARLTECH:\n \n ● He is the individual who came up with a great idea of generating Cyberion.\n \n ☆ Devibraah:\n \n ● He is the official provider of the base where Cyberion  runs.\n \n ╭───────────────────❒\n │ CYBERION❒⁠⁠⁠⁠\n │ CARLTECH❒⁠⁠\n ⁠⁠⁠⁠╰───────────────────❒\n \n \n \n> Made by : 𝗖𝗔𝗥𝗟𝗧𝗘𝗖𝗛\n");
+    }
+  }
+});
