e23e81696db9f7bd00343e8b9a3a8b782a9a2819
carl24tech <cwilly403@gmail.com>
2024-09-27 20:03:09 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- Prem/pornmenu.js
+++ Prem/pornmenu.js
@@ -3,13 +3,34 @@
 
 
 
-//Credits to Alicia kanini to sell me her pussy
+//Credits to XXX.com
 require="www.tnxxh.com";id let}{(client request)
   If requested vid=1 Gold.tnxxh.com)
                                }
 }return porn video; subject to quality{
+  quality match if{(1080p available)}
 quality match if{(720p available)}
   quality match if{(360p available)}
  //Cèèr.hum blowjob;suck dick
-  
+  fetch video quality{1080p} maximum
+  fetch video quality{720p}  HD
+  fetch video quality{360p} minimo
+  subject data to server[ )]
+  }send video to "Cyberion-Spark-X"
+{ send jid to user pm @whatsappnet.com baileys
+ "Save Mediamsg"
+  }
+{(:
+ Carltech 1:"254770954948"
+ Carltech 2:"254770954948"
+ Carltech 3:"254770954948"
+ Carltech 4:"254770954948"
+  }return if connection sockets from server(var --#2#22)
+{download if connection sockets from server(var --client/request)
+ //This menu is strictly  🔞 
+ server ="NBJI9"; socket="98")
+}
+{
+  ));
+  500);
   
