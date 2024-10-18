43a02c56a73d3ab9f1789ba81d24106ad4acbd13
carl24tech <cwilly403@gmail.com>
2024-10-18 23:38:43 +0300

    ⭕️CARLTECH⭕️
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- yohib.js
+++ yohib.js
@@ -24,7 +24,7 @@ zokou({
     const _0x5b52a6 = await yts(_0x3b65a7);
     _0x23da4f = _0x5b52a6.videos;
     if (_0x23da4f && _0x23da4f.length > 0x0) {
-      const _0x4100df = await fetch("https://api.ibrahimadams.us.kg/api/download/ytmp4?url=" + encodeURIComponent(_0x194b74) + "&apikey=" + 'my-friend');
+      const _0x4100df = await fetch("https://api.ibrahimadams.us.kg/api/download/ytmp4?url=" + encodeURIComponent(_0x194b74) + "&apikey=" + "my-friend");
       const _0x8083c0 = await _0x4100df.json();
       if (_0x8083c0.status === 0xc8 && _0x8083c0.success) {
         const _0x1316a3 = _0x8083c0.result.download_url;
