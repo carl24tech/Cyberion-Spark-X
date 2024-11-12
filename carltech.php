ce531c726f86a789d78525ad4d0a3eb2c6b6bff7
carl24tech <cwilly403@gmail.com>
2024-11-13 00:33:59 +0300

    ‌
    Signed-off-by: carl24tech <cwilly403@gmail.com>

---
+++ carltech.php
@@ -0,0 +1,17 @@
+**
+
+
+
+<?php
+// prevent Original code from unauthorised access 
+function greet($name) {
+    echo "Hello, $name!";
+}
+
+// Obfuscated code using a simple technique
+function _0x4142($name) {
+    echo "H\x65\x6c\x6c\x6f, $name!";
+}
+
+// Call the obfuscated function
+_0x4142("CarlTech");
