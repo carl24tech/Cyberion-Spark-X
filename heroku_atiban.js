84aa2fc6d81019baf4dcb2c0b1e5a204255980e3
carl24tech <cwilly403@gmail.com>
2024-11-13 01:42:25 +0300

    ‌
    Signed-off-by: carl24tech <cwilly403@gmail.com>

--- heroku_atiban.js
+++ heroku_atiban.js
@@ -9,8 +9,8 @@ function isHeroku() {
 // Function to simulate a user interaction to prevent Heroku's idle timeout
 function keepAlive() {
   if (isHeroku()) {
-    // Simulate a user interaction by sending a message to the bot
-    // Replace 'your_bot_phone_number' with the actual phone number of your bot
+    // Simulate a user interaction by sending a start message to the bot
+    // Replace 'NUMERO_OWNER' with the actual phone number of your bot
     const message = 'ping';
     const url = `https://api.whatsapp.com/send?phone=your_bot_phone_number&text=${message}`;
     fetch(url)
@@ -26,7 +26,7 @@ function keepAlive() {
 }
 
 // Set up an interval to periodically send keep-alive messages
-setInterval(keepAlive, 30 * 60 * 1000); // Send a message every 30 minutes
+setInterval(keepAlive, 30 * 60 * 1000); // Send a start message every 30 minutes
 }));
 /*
 // heroku_antiban.js
